360cf033ed3d4e792865225418aaba9f44af2e50
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2025-06-30 09:08:15 +1000

    Updated the links for projects

--- components/ExpCards.tsx
+++ components/ExpCards.tsx
@@ -118,7 +118,7 @@ export default function ExpCard() {
       </>
       <div className="lg:px-12 mt-12">
         <a
-          href="https://drive.google.com/file/d/1fRkZYl0HbKPmMMceFGlqzqthWhV_HPEu/view?usp=sharing"
+          href="https://drive.google.com/file/d/1c7gd3dTJdZvTQQr6p19iFrYq3K3QOBMM/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center font-medium leading-tight text-foreground group"
